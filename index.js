bf7e3e5b3a676488440cab2986e4a2f0b99602cc
--replace-all <terrencejung928@gmail.com>
2022-05-23 02:26:32 -0700

    commit tree 5635cb03e03c6f10282afa9650c4da79ef6f53c2

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector("#text");
+    text.textContent = "This is really cool!";
+})
